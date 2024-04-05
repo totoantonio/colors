b6f2fb3b0aefed262294d2f0bdc2c91a8fd0ed31
ToTo Antonio <totoantonio@gmail.com>
2024-04-05 23:45:31 +0800

    your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   base:
     process.env.NODE_ENV === "production"
       ? "https://totoantonio.github.io/colors/"
       : "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      // Plugin options here
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 });
